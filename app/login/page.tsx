f138e75e95e021dc647930a7ed037353b5589b3b
ohwojero godstime <ohwojerogodstime@gmail.com>
2025-11-10 14:01:58 +0100

    fixing the login issues for productions

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -12,8 +12,6 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { AlertCircle } from "lucide-react"
 import { Alert, AlertDescription } from "@/components/ui/alert"
-import { compare } from 'bcrypt'
-import { database as db } from '@/lib/db'
 import { authenticateUser } from './actions'
 
 
